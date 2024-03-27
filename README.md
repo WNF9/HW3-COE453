236af9b5865994c2d3afbc55ea62afe13c701738
WNF9 <116705013+WNF9@users.noreply.github.com>
2024-03-27 17:35:54 +0000

    lastModified

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # HW3-COE453
+
+
+## this is the the code for the calculator
---
+++ mul.js
@@ -0,0 +1,26 @@
+const functions = require('@google-cloud/functions-framework');
+
+// Register a function named 'mul' for HTTP trigger
+functions.http('mul', (req, res) => {
+  // Retrieve numbers 'x' and 'y' from the request query parameters or body
+  let x = parseInt(req.query.x) || parseInt(req.body.x);
+  let y = parseInt(req.query.y) || parseInt(req.body.y);
+
+  // Check if 'x' and 'y' are both integers
+  if (isNaN(x) || isNaN(y)) {
+    res.status(400).send('The values of x and y must be integers.');
+    return;
+  }
+
+  // Further check if the parsed values are indeed integers and not just numeric values
+  if (!Number.isInteger(x) || !Number.isInteger(y)) {
+    res.status(400).send('The values of x and y must be integers without decimal places.');
+    return;
+  }
+
+  // Perform addition
+  let result = x * y;
+
+  // Respond with a JSON object containing 'x', 'y', and the 'result'
+  res.json({ x, y, result });
+});
